ae2ecc292baaba621bc7eb87122ca810498db0da
Alstocras <alstoperiment@gmail.com>
2025-12-08 13:52:37 +0530

    basic JS, someone needs to add HTML and CSS

---
+++ index.js
@@ -0,0 +1,24 @@
+const aTag = document.getElementById("coefficientA");
+const bTag = document.getElementById("coefficientB"); 
+const cTag = document.getElementById("coefficientC");
+const solutionToggleTag = document.getElementById("solTog");
+
+function solveForX(){
+   let a = 3//parseFloat(aTag.value); 
+   let b = 7//parseFloat(bTag.value); 
+   let c = 8//parseFloat(cTag.value);
+   let solutionToggle = 3//parseFloat(solutionToggleTag.value);
+
+   let result1 = (-1 * b + math.sqrt((b ** 2) - 4 * a * c))/(2 * a);
+   let result2 = (-1 * b - math.sqrt((b ** 2) - 4 * a * c))/(2 * a);
+
+   if(solutionToggle == 2){
+    alert(result2);
+   }
+   else if(solutionToggle == 1){
+    alert(result1);
+   }
+   else{
+    alert(result1 + " and " + result2);
+   }
+}
